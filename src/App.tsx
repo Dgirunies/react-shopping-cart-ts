2d4d11cecac69351fd3e56dd7045d5fa07def73a
Dgirunies <erineu16@gmail.com>
2022-04-23 00:22:21 +0100

    Adding First components and fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+//Components
+import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+//Styles
+import { Wrapper } from "./App.styles";
+
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
+
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">Start</header>
-    </div>
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
   );
+  console.log(data);
+  return <div className="App">Start</div>;
 };
 
 export default App;
